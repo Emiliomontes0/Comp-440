65b17d085d2559de1cd7218007b68e00b352a839
Emilio <emiliomontes10@gmail.com>
2025-03-12 09:04:01 -0700

    fix server start and created migrations

--- README.md
+++ README.md
@@ -8,3 +8,7 @@
     - cd .. (to go back to root)
   - cd frontend (go into frontend folder)
     - npm install (installs all decpency)
+
+- **How to run server**
+  - backend use (npm start)
+  - frontend use (npm run dev)
---
+++ backend/models/user.js
@@ -0,0 +1,42 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: true
+      }
+    },
+    {
+      tableName: "users",
+    }
+  );
+
+  return User;
+};
+
