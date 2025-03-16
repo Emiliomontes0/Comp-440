f30a1483dec81da8ad2db928aff52c8680f2f5f4
Emilio <emiliomontes10@gmail.com>
2025-03-15 21:33:23 -0700

    create signup endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,7 @@ app.use(cors());
 app.get("/", (req, res) => {
     res.send("API is running...");
 });
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
